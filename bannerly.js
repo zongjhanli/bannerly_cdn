397c0831ca3b6004f52dd0110d05551a74ba65e3
Zong Jhan Li <zjli1993@gmail.com>
2021-07-11 15:07:36 +0800

    commit @Basic Entries js code

--- bannerly.js
+++ bannerly.js
@@ -1,6 +1,6 @@
 // 此JS文件主要為Bannerly做了Webflow Designer無法支援的JS Events細部功能設定
 
-//////// 以下為comment前綴說明
+//////// comment前綴說明
 // "GLOBAL" -> 應用於全站所有相關物件
 // "GLOBAL/@pagename" -> 為了特定頁面之相關物件從「全站通用」衍生出的區塊
 // "@pagename" -> 只應用於某特定頁面的相關物件
@@ -119,3 +119,33 @@ callers.forEach((caller) => {
 
     })
 })
+
+// ----------------------------------------------------------------------------------------------------
+
+// @Basic-Entries
+// datepicker.js function (區間日期選項)
+$('#date-exposure_range').daterangepicker({
+    ranges: {
+        '今天': [moment(), moment()],
+        '下一週': [moment().add(1, 'week').startOf('week'), moment().add(1, 'week').endOf('week')],
+        '下一月份': [moment().add(1, 'month').startOf('month'), moment().add(1, 'month').endOf('month')],
+    },
+    "alwaysShowCalendars": true,
+    "startDate": moment(),
+    "endDate": moment().add(0, 'week').endOf('week'),
+    "minDate": moment(),
+    "opens": "center",
+}, function(start, end, label) {});
+
+// datepicker.js function (單一日期選項)            
+$('#date-deadline').daterangepicker({
+    "singleDatePicker": true,
+    "alwaysShowCalendars": true,
+    "startDate": moment(),
+    "endDate": moment(),
+    "minDate": moment(),
+    "opens": "center",
+    "drops": "up"
+}, function(start, label) {});
+
+// ----------------------------------------------------------------------------------------------------
