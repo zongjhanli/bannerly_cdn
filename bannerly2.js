847c1f6119ddec5497c40a66380131fb8d5cd857
Zong Jhan Li <zjli1993@gmail.com>
2021-10-02 10:04:28 +0800

    dropdown input

--- bannerly2.js
+++ bannerly2.js
@@ -120,3 +120,343 @@ $('.js-length-10').on('input', function(e) {
 });
 
 // ----------------------------------------------------------------------------------------------------
+
+//Global multi-dropdown input value
+document.addEventListener('click', (e) => {
+    let target = e.target;
+    e.preventDefault();
+    let multiDropCards = document.querySelectorAll('.drop-card[data-drop=multi]');
+    for (const multiDropCard of multiDropCards) {
+        let shownGroup = multiDropCard.querySelector('.drop-group.js-show');
+        let checkCount = shownGroup.querySelectorAll('.js-selected').length;
+        let input = multiDropCard.parentElement.querySelector('.input.dropdown');
+
+        if (target.dataset.drop == 'multi' && input == document.activeElement) {
+            input.value = '';
+        } else if (input != document.activeElement) {
+            if (checkCount == '0') {
+                input.value = '';
+            } else {
+                input.value = '已選' + checkCount + '項';
+            }
+        }
+    }
+});
+
+
+//Global general dropdown behaviours
+document.addEventListener('click', (e) => {
+    let target = e.target;
+    let dropCards = document.querySelectorAll('.drop-card');
+    for (const dropCard of dropCards) {
+        let dropArrow = dropCard.parentElement.querySelector('.dropdown-arrow');
+
+        function globalCollapse() {
+            dropCard.classList.add('js-collapsed');
+            dropArrow.classList.remove('js-rotated');
+            dropArrow.classList.add('unclickable');
+        };
+
+        function expand() {
+            target.parentElement.querySelector('.drop-card').classList.remove('js-collapsed');
+            target.parentElement.querySelector('.dropdown-arrow').classList.add('js-rotated');
+            target.parentElement.querySelector('.dropdown-arrow').classList.remove('unclickable');
+        };
+
+        function expand2() {
+            target.parentElement.parentElement.parentElement.classList.remove('js-collapsed');
+            target.parentElement.parentElement.parentElement.parentElement.querySelector('.dropdown-arrow').classList.add('js-rotated');
+            target.parentElement.parentElement.parentElement.parentElement.querySelector('.dropdown-arrow').classList.remove('unclickable');
+        };
+
+        if (!dropCard.classList.contains('js-collapsed')) {
+            globalCollapse();
+        }; //無論點選何處，dropCard預設全數收回
+
+        if (target.classList.contains('input', 'dropdown')) {
+            expand(); //點選input時 -> dropdown開啟
+        }
+
+        if (dropCard.dataset.drop == 'multi' &&
+            target.classList.contains('full-touch') &&
+            !target.classList.contains('js-exclude')) {
+            expand2();
+        } else if (dropCard.dataset.drop == 'single' &&
+            target.classList.contains('full-touch') &&
+            !target.classList.contains('js-exclude')) {
+            globalCollapse(); //!-- 尚未考慮使用tab切換選項的使用情境
+        }
+    }
+});
+
+
+//input輸入時/輸入後響應
+let dropInputs = document.querySelectorAll('.input.dropdown');
+for (const dropInput of dropInputs) {
+    let dropCard = dropInput.parentElement.querySelector('.drop-card');
+    dropInput.addEventListener('input', (e) => {
+        //e.preventDefault();
+        let target = e.target;
+        let capInput = target.value.toUpperCase();
+
+        //選項隨著輸入值對照而進一步顯示or隱藏
+        let dropOptions = dropCard.querySelectorAll('.a-button.as-list');
+        for (const dropOption of dropOptions) {
+            let label = dropOption.querySelector('div.label');
+            let capLabel = label.textContent.toUpperCase();
+
+            dropOption.classList.add('js-hide');
+
+            if (capLabel.includes(capInput)) {
+                dropOption.classList.remove('js-hide');
+            }
+        }
+    })
+
+    //輸入後若無match字符 -> 新增選項
+    let timer,
+        timeoutVal = 0; // time it takes to wait for user to stop typing in ms
+
+    // detects when the user is actively typing
+    //dropInput.addEventListener('keypress', handleKeyPress);
+    // triggers a check to see if the user is actually done typing
+    dropInput.addEventListener('keyup', handleKeyUp);
+
+    function newOption() {
+        let keyInText = dropInput.value;
+        let dropGroup = dropCard.querySelector('.drop-group.js-show');
+        let button = document.createElement('div');
+        let label = document.createElement('div');
+        let checker = document.createElement('div');
+
+        button.appendChild(label);
+        button.appendChild(checker);
+        button.classList.add('a-button', 'as-list');
+        label.classList.add('label', 'full-touch');
+        checker.classList.add('custom-check', 'tick-right');
+        label.textContent = '新增' + '「' + keyInText + '」';
+        dropGroup.insertBefore(button, dropGroup.children[0]);
+        label.dataset.custom = 'pending';
+
+
+        //新增於document的選項需要重新設定點按響應
+        document.addEventListener('click', (e) => {
+                let target = e.target;
+                let tChecker = target.nextElementSibling;
+                let tDropCard = target.parentElement.parentElement.parentElement;
+                let otherChecker = tDropCard.querySelector('.js-selected'); //for single
+
+
+                function revealAll() {
+                    let tOptions = dropInput.parentElement.querySelector('.drop-group.js-show').querySelectorAll('.a-button.as-list');
+                    for (const tOption of tOptions) {
+                        tOption.classList.remove('js-hide');
+                    }
+                }
+
+                function confirmAppended() {
+                    tChecker.classList.add('js-selected');
+                    let tNewStr = target.textContent.replace('新增「', '').replace('」', '');
+                    target.textContent = tNewStr;
+                    dropInput.value = tNewStr;
+                    target.dataset.custom = 'confirmed';
+                }
+
+                if (target.classList.contains('label') && !tChecker.classList.contains('js-selected')) {
+                    confirmAppended();
+                    revealAll();
+                    if (tDropCard.dataset.drop == 'single' &&
+                        otherChecker.classList.contains('js-selected')) {
+                        otherChecker.classList.remove('js-selected');
+                    } else {
+                        return;
+                    }
+                }
+
+                //清除殘存的「未選選選項」
+                if (document.querySelector('[data-custom=pending]') != null) {
+                    document.querySelector('[data-custom=pending]').parentElement.remove();
+                }
+            }) //end of document click event
+
+    } //end of newOption()
+
+    function handleKeyUp(e) {
+        window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
+        timer = window.setTimeout(() => {
+            if (dropInput.value != '') {
+                newOption();
+            }
+            let dropInputConcated = dropInput.value.replace(dropInput.value, '').concat('新增「', dropInput.value, '」');
+            let pendingOptions = dropCard.querySelectorAll('[data-custom=pending]');
+            let existingOptions = dropCard.querySelectorAll('.label:not([data-custom=pending])');
+            existingOptions.forEach((existingOption) => {
+                pendingOptions.forEach((pendingOption) => {
+                    let pendingOptionText = pendingOption.textContent;
+                    if (dropInput.value == '') {
+                        pendingOption.parentElement.remove(); //刪掉所有input字符後->刪
+                    } else if (pendingOptionText.indexOf(dropInputConcated) == -1) {
+                        pendingOption.parentElement.remove(); //刪刪改改後還是與新增建議同名者->刪
+                    } else if (existingOption.textContent.includes(dropInput.value)) {
+                        pendingOption.parentElement.remove(); //與既存選項同名者->刪
+                    }
+                })
+            })
+        }, timeoutVal);
+    } //end of handleKeyUp()
+
+    dropInput.addEventListener('focus', (e) => {
+        let target = e.target;
+        target.select();
+        let tOptions = dropInput.parentElement.querySelector('.drop-group.js-show').querySelectorAll('.a-button.as-list');
+        for (const tOption of tOptions) {
+            tOption.classList.remove('js-hide');
+        }
+    })
+
+} //end of dropInput loop !!!
+
+//dropdown假選項點擊響應
+let fakeBtns = document.querySelectorAll('.label.full-touch:not(.js-exclude)');
+for (const fakeBtn of fakeBtns) {
+    fakeBtn.addEventListener('click', (e) => {
+            let target = e.target;
+            let checker = target.parentElement.querySelector('.custom-check');
+            let tGroup = target.parentElement.parentElement;
+            let tDPBox = target.parentElement.parentElement.parentElement.parentElement;
+            let tInput = tDPBox.querySelector('.input.dropdown');
+            let tDropCard = tInput.parentElement.querySelector('.drop-card');
+
+            //多選選項 (預設)
+            if (!checker.classList.contains('js-selected')) {
+                checker.classList.add('js-selected');
+                target.dataset.select = 'true';
+            } else {
+                checker.classList.remove('js-selected');
+                target.dataset.select = '';
+            }
+
+            //多選選項value同步至textarea
+            let tTextAreas = tDPBox.parentElement.parentElement.querySelectorAll('textarea');
+            for (const tTextArea of tTextAreas) {
+                if (tDropCard.dataset.drop == 'multi' && tGroup.dataset.group === tTextArea.dataset.name &&
+                    tInput.dataset.drop != 'ec') {
+                    let multiSelecteds = tGroup.querySelectorAll('[data-select=true]');
+                    let TextStr = Array.from(multiSelecteds, x => x.textContent);
+                    tTextArea.value = TextStr.join('\n');
+                }
+            }
+
+            // 單選選項專用響應 -> 模擬radio input
+            if (checker.classList.contains('js-selected') && tDropCard.dataset.drop == 'single') {
+                let allCheckers = tDropCard.querySelectorAll('.custom-check');
+                for (const allChecker of allCheckers) {
+                    allChecker.classList.remove('js-selected');
+                }
+                checker.classList.add('js-selected');
+            }
+
+            // 單選input專用響應 -> input value 不累加
+            if (checker.classList.contains('js-selected') && tInput.dataset.drop == 'single') {
+                tInput.value = target.textContent;
+            } else {
+                tInput.value = '';
+            }
+
+            // ec 尺寸數量同步
+            let sizeCount = tGroup.querySelectorAll('.js-selected').length;
+            let ecTabs = document.querySelectorAll('.label[data-tab]');
+            for (const ecTab of ecTabs) {
+                let countResult = ecTab.nextElementSibling;
+                if (target.dataset.ec == null) {
+                    if (tGroup.dataset.group === ecTab.dataset.tab) {
+                        countResult.textContent = sizeCount;
+                    }
+                }
+            }
+
+            //ec 多選專用響應
+            let tTabs = tDPBox.parentElement.parentElement.querySelectorAll('.label[data-tab]');
+            for (const tTab of tTabs) {
+                if (target.dataset.ec === tTab.dataset.tab) {
+                    if (checker.classList.contains('js-selected')) {
+                        tTab.parentElement.classList.add('js-show');
+                    } else if (!checker.classList.contains('js-selected')) {
+                        tTab.parentElement.classList.remove('js-show');
+                    }
+                }
+            }
+        }) //end of fakeButton click event
+} //end of fakeButton loop
+
+//tab + indicator 響應
+let ecTabsCols = document.querySelectorAll('[data-col=ec-tab]');
+for (const ecTabsCol of ecTabsCols) {
+    ecTabsCol.addEventListener('click', (e) => {
+            let target = e.target;
+            let indicator = ecTabsCol.querySelector('.indicator'); //! 不需要使用target.querySelector
+            let shownTabs = ecTabsCol.querySelectorAll('.a-button.js-show');
+            let tabLength = shownTabs.length;
+
+            //ec tab indicator 顯示/隱藏條件
+            if (tabLength > 0) {
+                indicator.style.display = 'block';
+            } else {
+                indicator.style.display = 'none';
+            }
+
+            for (const shownTab of shownTabs) {
+
+                // ec drop option 點擊響應 -> 預設第一個tab以及相關右欄顯現
+                if (target.nextElementSibling.classList.contains('custom-check')) {
+                    shownTabs[0].style.opacity = '1';
+                    let tCol = target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+                    let tDropGroups = tCol.nextElementSibling.querySelectorAll('.drop-group');
+                    for (const tDropGroup of tDropGroups) {
+                        tDropGroup.classList.remove('js-show');
+                        if (tDropGroup.dataset.group === shownTabs[0].firstElementChild.dataset.tab) {
+                            tDropGroup.classList.add('js-show');
+                        }
+                    }
+                    let tTextAreas = tCol.nextElementSibling.querySelectorAll('.as-textarea');
+                    for (const tTextArea of tTextAreas) {
+                        tTextArea.classList.remove('js-show');
+                        if (tTextArea.dataset.name === shownTabs[0].firstElementChild.dataset.tab) {
+                            tTextArea.classList.add('js-show');
+                        }
+                    }
+                }
+
+                // ec tab 點擊響應
+                if (target.dataset.tab != null) {
+                    shownTab.style.opacity = '0.5';
+                    target.parentElement.style.opacity = '1';
+
+                    let tCol = target.parentElement.parentElement.parentElement;
+                    let tDropGroups = tCol.nextElementSibling.querySelectorAll('.drop-group');
+                    for (const tDropGroup of tDropGroups) {
+                        tDropGroup.classList.remove('js-show');
+                        if (tDropGroup.dataset.group === target.dataset.tab) {
+                            tDropGroup.classList.add('js-show');
+                        }
+                    }
+                    let tTextAreas = tCol.nextElementSibling.querySelectorAll('.as-textarea');
+                    for (const tTextArea of tTextAreas) {
+                        tTextArea.classList.remove('js-show');
+                        if (tTextArea.dataset.name === target.dataset.tab) {
+                            tTextArea.classList.add('js-show');
+                        }
+                    }
+
+                    //tab 切換 -> indicator移動
+                    for (i = 0; i < tabLength; i++) {
+                        let topV = i * 36 + "px";
+                        if (shownTabs[i] == target.parentElement) {
+                            indicator.style.top = topV;
+                        }
+                    }
+                }
+
+            } // end of shownTab loop
+        }) //end of ecTabsCol click event
+} //end of ecTabsCol loop
