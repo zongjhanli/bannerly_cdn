afa0838cb45b0cb964c2960ef8ae7f4dbb927de1
Zong Jhan Li <zjli1993@gmail.com>
2021-10-03 14:04:48 +0800

    newTab()

--- bannerly2.js
+++ bannerly2.js
@@ -405,9 +405,6 @@ for (const dropInput of dropInputs) {
             //for (const pendingOption of pendingOptions) {
             if (dropInput.value == '') {
                 pendingOptions[0].parentElement.remove(); //刪掉所有input字符後->刪
-            } else if (pendingStrUpper.indexOf(inputConcatedUpper) != -1) {
-                console.log('a');
-                pendingOptions[0].parentElement.remove(); //刪刪改改後還是與新增建議同名者->刪
             } else if (existingStrUpper.indexOf(dropInputUpper) != -1) {
                 console.log('b');
                 pendingOptions[0].parentElement.remove(); //與既存選項同名者->刪
