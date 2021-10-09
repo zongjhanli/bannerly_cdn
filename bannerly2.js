aab2c3825f0521f722af955a96895abbf9ad3c9f
Zong Jhan Li <zjli1993@gmail.com>
2021-10-09 14:49:39 +0800

    card resize

--- bannerly2.js
+++ bannerly2.js
@@ -305,31 +305,36 @@ document.addEventListener('click', (e) => {
     }) //end of document click event
 
 //案型增減區
-// window.onload = function() {
-//     let resizedBoxes = document.querySelectorAll('.card-box.js-resize');
-//     for (const resizedBox of resizedBoxes) {
-//         resizedBox.style.display = 'none';
 
-//         let add = document.querySelector('.js-add');
-//         add.addEventListener('click', () => {
-//             resizedBoxes[0].style.display = 'none';
-//         })
-//     }
-// };
+let sections = document.querySelectorAll('#Copywright, #Product, #Size');
+for (const section of sections) {
+    window.onload = function() {
+        let resizedBoxes = section.querySelectorAll('.card-box.js-resize');
+        for (const resizedBox of resizedBoxes) {
+            resizedBox.style.display = 'none';
+        }
+    }
 
-document.addEventListener('click', (e) => {
-        let target = e.target;
+    section.addEventListener('click', (e) => {
+            let target = e.target;
 
-        // card resize -> target=.card-cap
-        let tCard = target.parentElement;
-        let tSection = target.parentElement.parentElement.parentElement;
-        let cards = tSection.querySelectorAll('.card');
-        for (const card of cards) {
-            let cardBox = card.parentElement;
-            let tCardBox = tCard.parentElement;
+            // 新增案型
+            if (target.classList.contains('js-add')) {
+                let hiddenBoxes = section.querySelectorAll('.card-box.js-resize');
+                hiddenBoxes[0].style.display = 'block';
+                if (hiddenBoxes == null) {
+                    console.log('no-more');
+                }
+            }
 
-            if (target.classList.contains('card-cap') || target.parentElement.classList.contains('card-cap')) {
-                if (!target.classList.contains('js-remove')) { //排除「刪除案型」按鈕
+            // card resize -> target=.card-cap
+            if (target.classList.contains('card-cap')) {
+                let tCard = target.parentElement;
+                let tSection = target.parentElement.parentElement.parentElement;
+                let cards = tSection.querySelectorAll('.card');
+                for (const card of cards) {
+                    let cardBox = card.parentElement;
+                    let tCardBox = tCard.parentElement;
                     card.classList.add('js-resize');
                     cardBox.classList.add('js-resize');
 
@@ -338,10 +343,10 @@ document.addEventListener('click', (e) => {
                         tCardBox.classList.remove('js-resize');
                     }
                 }
-            }
-        } //end of card resize
-
-    }) //end of 案型增減區
+            } //end of card resize
+        }) //end of section click
+}
+//end of 案型增減區
 
 //input輸入時/輸入後響應
 let dropInputs = document.querySelectorAll('.input.dropdown');
